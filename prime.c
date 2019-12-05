4770d484e1560e0a76121597051f958dfe8b5c34
palindrome-nit <https://github.com/palindrome-nit/codenvy.git>
2019-12-05 07:10:21 +0000

    final commit

--- prime.c
+++ prime.c
@@ -15,9 +15,9 @@ int main() {
     }
     else {
         if (flag == 0)
-            printf("%d is a prime number.", n);
+            printf("%d is a prime number:", n);
         else
-            printf("%d is not a prime number.", n);
+            printf("%d is not a prime number:", n);
     }
     return 0;
 }
